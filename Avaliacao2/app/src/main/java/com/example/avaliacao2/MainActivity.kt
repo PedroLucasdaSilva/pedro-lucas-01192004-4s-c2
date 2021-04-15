22256b350669eb4b629e8bb4ac8b06f86e8d8342
PedroLucasdaSilva <pedro.lsilva@bandtec.com.br>
2021-04-14 21:48:22 -0300

    Shared preferences não funciona

--- Avaliacao2/app/src/main/java/com/example/avaliacao2/MainActivity.kt
+++ Avaliacao2/app/src/main/java/com/example/avaliacao2/MainActivity.kt
@@ -37,64 +37,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun comprarCachorros(view: View) {
-//        val etCachorro1: EditText = findViewById(R.id.et_id1)
-//        val etCachorro2: EditText = findViewById(R.id.et_id2)
-//        val edit = preferencias.edit()
-//        edit.putString("idCachorro1",etCachorro1.text.toString())
-//        edit.putString("idCachorro2",etCachorro2.text.toString())
-//        edit.commit()
+        val etCachorro1: EditText = findViewById(R.id.et_id1)
+        val etCachorro2: EditText = findViewById(R.id.et_id2)
+        val edit = preferencias.edit()
+        edit.putInt("idCachorro1",etCachorro1.text.toString().toInt())
+        edit.putInt("idCachorro2",etCachorro2.text.toString().toInt())
+        edit.commit()
 
-        var valor1:Int = 0
-        var erro:String ="Deu ruim... Nenhum cachorro encontrado o(s) id(s) "
-        val tvCompra: TextView = findViewById(R.id.tv_compra)
+//        var valor1:Int = 0
+//        var erro:String ="Deu ruim... Nenhum cachorro encontrado o(s) id(s) "
+//        val tvCompra: TextView = findViewById(R.id.tv_compra)
+//
+//        val apiCachorros = CachorrosConexao.criar()
+//        val etId1: EditText = findViewById(R.id.et_id1)
+//        val etId2: EditText = findViewById(R.id.et_id2)
+//        val id1 = etId1.text.toString().toInt()
+//        val id2 = etId2.text.toString().toInt()
+//
+//        apiCachorros.get(id1).enqueue(object : Callback<Cachorros> {
+//            override fun onFailure(call: Call<Cachorros>, t: Throwable) {
+//               tvCompra.text = "Deu ruim... Nenhum cachorro encontrado o(s) id(s) ${id1}"
+//            }
+//
+//            override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
+//                val tvCompra1:TextView = findViewById(R.id.et_id1)
+//                val cachorros = response.body()
+//                if (cachorros!=null){
+//                    valor1 = cachorros.precoMedio
+//                    tvCompra.text =  "Cachorro 1: ${cachorros.precoMedio}\n"
+//                } else {
+//                    erro = erro.toString() + "${id1}"
+//                    tvCompra.text = erro +"\n"
+//                }
+//            }
+//        })
+//        apiCachorros.get(id2).enqueue(object : Callback<Cachorros> {
+//            override fun onFailure(call: Call<Cachorros>, t: Throwable) {
+//                tvCompra.text = tvCompra.text.toString() + "${id2}"
+//            }
+//
+//            override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
+//                val tvCompra1:TextView = findViewById(R.id.et_id1)
+//                val cachorros = response.body()
+//                val soma:Int
+//                if (cachorros!=null){
+//
+//                    soma = cachorros.precoMedio + valor1
+//                    tvCompra.text = tvCompra.text.toString() +  "Cachorro 2: ${cachorros.precoMedio}\n" +
+//                            "Valor Total: ${soma}\n"
+//
+//
+//                } else {
+//                    erro = erro.toString() + "${id2}"
+//                    tvCompra.text = tvCompra.text.toString() + erro + "\n"
+//                }
+//            }
+//        })
 
-        val apiCachorros = CachorrosConexao.criar()
-        val etId1: EditText = findViewById(R.id.et_id1)
-        val etId2: EditText = findViewById(R.id.et_id2)
-        val id1 = etId1.text.toString().toInt()
-        val id2 = etId2.text.toString().toInt()
-
-        apiCachorros.get(id1).enqueue(object : Callback<Cachorros> {
-            override fun onFailure(call: Call<Cachorros>, t: Throwable) {
-               tvCompra.text = "Deu ruim... Nenhum cachorro encontrado o(s) id(s) ${id1}"
-            }
-
-            override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
-                val tvCompra1:TextView = findViewById(R.id.et_id1)
-                val cachorros = response.body()
-                if (cachorros!=null){
-                    valor1 = cachorros.precoMedio
-                    tvCompra.text =  "Cachorro 1: ${cachorros.precoMedio}\n"
-                } else {
-                    erro = erro.toString() + "${id1}"
-                    tvCompra.text = erro +"\n"
-                }
-            }
-        })
-        apiCachorros.get(id2).enqueue(object : Callback<Cachorros> {
-            override fun onFailure(call: Call<Cachorros>, t: Throwable) {
-                tvCompra.text = tvCompra.text.toString() + "${id2}"
-            }
-
-            override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
-                val tvCompra1:TextView = findViewById(R.id.et_id1)
-                val cachorros = response.body()
-                val soma:Int
-                if (cachorros!=null){
-
-                    soma = cachorros.precoMedio + valor1
-                    tvCompra.text = tvCompra.text.toString() +  "Cachorro 2: ${cachorros.precoMedio}\n" +
-                            "Valor Total: ${soma}\n"
-
-
-                } else {
-                    erro = erro.toString() + "${id2}"
-                    tvCompra.text = tvCompra.text.toString() + erro + "\n"
-                }
-            }
-        })
-
-//        val compra = Intent(this, compra::class.java)
-//        startActivity(compra)
+        val compra = Intent(this, compra::class.java)
+        startActivity(compra)
     }
 }
--- Avaliacao2/app/src/main/java/com/example/avaliacao2/compra.kt
+++ Avaliacao2/app/src/main/java/com/example/avaliacao2/compra.kt
@@ -15,47 +15,53 @@ class compra : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compra)
+        val cachorro1 = intent.getIntExtra("idCachorro1",0)
+        val cachorro2 = intent.getIntExtra("idCachorro2",0)
+
+
         var valor1:Int = 0
-        val cachorro1 = intent.getStringExtra("idCachorro1")
-        val cachorro2 = intent.getStringExtra("idCachorro2")
+        var erro:String ="Deu ruim... Nenhum cachorro encontrado o(s) id(s) "
         val tvCompra: TextView = findViewById(R.id.tv_compra)
+
         val apiCachorros = CachorrosConexao.criar()
-        val etId1: EditText = findViewById(R.id.et_id1)
-        val etId2: EditText = findViewById(R.id.et_id2)
-        val id1 = etId1.text.toString().toInt()
-        val id2 = etId2.text.toString().toInt()
+
+        val id1 = cachorro1.toString().toInt()
+        val id2 = cachorro2.toString().toInt()
+
         apiCachorros.get(id1).enqueue(object : Callback<Cachorros> {
             override fun onFailure(call: Call<Cachorros>, t: Throwable) {
-               tvCompra.text = "Deu ruim... Nenhum cachorro encontrado o(s) id(s) ${id1}"
+                tvCompra.text = "Deu ruim... Nenhum cachorro encontrado o(s) id(s) ${id1}"
             }
 
             override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
-                val tvCompra1:TextView = findViewById(R.id.et_id1)
                 val cachorros = response.body()
-                if (cachorros!= null){
+                if (cachorros!=null){
                     valor1 = cachorros.precoMedio
-                    tvCompra.text =tvCompra.text.toString() + "Cachorro 1: ${cachorros.precoMedio}\n"
+                    tvCompra.text =  "Cachorro 1: ${cachorros.precoMedio}\n"
                 } else {
-                    tvCompra.text = ""
+                    erro = erro.toString() + "${id1}"
+                    tvCompra.text = erro +"\n"
                 }
             }
         })
         apiCachorros.get(id2).enqueue(object : Callback<Cachorros> {
             override fun onFailure(call: Call<Cachorros>, t: Throwable) {
-                tvCompra.text =tvCompra.text.toString() + "${id2}"
+                tvCompra.text = tvCompra.text.toString() + "${id2}"
             }
 
             override fun onResponse(call: Call<Cachorros>, response: Response<Cachorros>) {
-                val tvCompra1:TextView = findViewById(R.id.et_id1)
                 val cachorros = response.body()
                 val soma:Int
                 if (cachorros!=null){
 
                     soma = cachorros.precoMedio + valor1
-                    tvCompra.text = tvCompra.text.toString() + "Cachorro 2: ${cachorros.precoMedio}\n" +
-                                                            "Valor Total: ${soma}"
+                    tvCompra.text = tvCompra.text.toString() +  "Cachorro 2: ${cachorros.precoMedio}\n" +
+                            "Valor Total: ${soma}\n"
+
+
                 } else {
-                    tvCompra.text = ""
+                    erro = erro.toString() + "${id2}"
+                    tvCompra.text = tvCompra.text.toString() + erro + "\n"
                 }
             }
         })
